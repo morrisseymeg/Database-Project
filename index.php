b43aec760603f3bf99dc97922e6b4bbcdd859e9b
morrisseymeg <meg.morrissey@gmail.com>
2014-11-15 09:46:36 -0500

    Log In
    Log In page for scheduler

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
 <?php
-	echo "hello world";
+require_once "pdo.php";
+session_start();
 ?>
+<html><head><title>Welcome and Log In</title></head>
+<body>
+<?php
+if (isset($_SESSION['error']) ) {
+    echo '<p style="color:red">'.$_SESSION['error']."</p>\n";
+    unset($_SESSION['error']);
+}
+if (isset($_SESSION['success']) ) {
+    echo '<p style="color:green">'.$_SESSION['success']."</p>\n";
+    unset($_SESSION['success']);
+}
+?>
+</table>
+<a href="login.php">Log In</a>
+
