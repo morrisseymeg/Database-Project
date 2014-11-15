1662caa42601d0ddccffaa07149065a12f97b296
xihuang <huangxi@umich.edu>
2014-11-15 12:54:51 -0500

    login working!! its working!!!!
    fixed login to check password

--- index.php
+++ index.php
@@ -1,23 +1,28 @@
 <?php
+    require_once "pdo.php";
     session_start();
-    if ( isset($_POST["account"]) && isset($_POST["pw"]) ) {
-        unset($_SESSION["account"]);  // Logout current user
-        if ( $_POST['pw'] == '***' ) { //need password/user database
-            $_SESSION["account"] = $_POST["account"];
-            $_SESSION["success"] = "Logged in.";
-            header( 'Location: index.php' ) ;
-            return;
-        } else {
-            $_SESSION["error"] = "Incorrect password.";
-            header( 'Location: login.php' ) ;
-            return;
-        }
-    }      
-    $stmt = $pdo->query("SELECT id, uniqname, pw FROM userinfo");
+    if ( isset($_POST["uniqname"]) && isset($_POST["pw"]) ) {
+        // unset($_SESSION["account"]);  // Logout current user
+        // if ( $_POST['pw'] == '***' ) { //need password/user database
+        //     $_SESSION["account"] = $_POST["account"];
+        //     $_SESSION["success"] = "Logged in.";
+        //     header( 'Location: index.php' ) ;
+        //     return;
+        // } else {
+        //     $_SESSION["error"] = "Incorrect password.";
+        //     header( 'Location: login.php' ) ;
+        //     return;
+        // }
+        
+    $stmt = $pdo->query("SELECT uniqname, pw FROM userinfo");
     while ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {  
         if ($_POST['uniqname'] == $row['uniqname'] && $_POST['pw'] == $row['pw']){
             header('Location: main.php');
         }
+        else{
+            $_SESSION['error'] = "Your uniqname or password is incorrect.";
+        }
+    }
     }  
 ?>
 <html>
