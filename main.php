c2ae8b6ef7b8a4cbbdfbd3b8d1752ff6b6c957ea
morrisseymeg <meg.morrissey@gmail.com>
2014-11-23 13:44:22 -0500

    Main can add days
    Updated main.php to allow info to go into the days table.

--- main.php
+++ main.php
@@ -1,3 +1,21 @@
-<?php 
-	echo "hello, I am main.";
-	?>
+<?php
+require_once "pdo.php";
+session_start();
+
+if ( isset($_POST['day'])) {
+     
+     $sql = "INSERT INTO days (day) 
+              VALUES (:day)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute(array(
+        ':day' => $_POST['day'],
+   $_SESSION['success'] = 'Record Added';
+   header( 'Location: index.php' ) ;
+   return;
+}
+//Flash pattern
+if (isset($_SESSION['error']) ) {
+    echo '<p style="color:red">'.$_SESSION['error']."</p>\n";
+    unset($_SESSION['error']);
+}
+?>
