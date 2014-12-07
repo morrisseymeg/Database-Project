72d2c9701c034043eee1ef8a4b918f8df7bc6608
morrisseymeg <meg.morrissey@gmail.com>
2014-12-06 19:14:00 -0500

    sql statement
    sql statement added to file

--- AddtoDatabase.php
+++ AddtoDatabase.php
@@ -1,6 +1,16 @@
 <?php
-if (isset($_POST['abc'])) {
-	echo $_POST['abc'];
+if (isset($_POST['avail'])) {
+    $sql = "INSERT into avail (day_ID, avail, time_ID, user_ID, cell_ID)
+        VALUES (:day_ID, :avail, :time_ID, :user_ID, :cell_ID)";
+    $stmt = $pdo->prepare($sql);
+        $stmt->execute(array(
+        ':day_ID, :avail, :time_ID, :user_ID, :cell_ID' => $_POST['day_ID, avail, time_ID, user_ID, cell_ID']));
+	echo $_POST['avail'];
 	return;
 }
+
 ?>
+
+
+//add sql statement
+//how can we connect dataConnect to the AddtoDatabase?
