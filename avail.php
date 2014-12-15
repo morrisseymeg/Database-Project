b1c992a362a15429c16288fdd872d6c9ef013601
xihuang <huangxi@umich.edu>
2014-12-14 23:35:33 -0500

    avail displaying group availability
    displaying

--- avail.php
+++ avail.php
@@ -2,7 +2,7 @@
 require_once "pdo.php";
 session_start();
 $user_ID = $_SESSION['user_id'];
-echo "Generating your group's availability";
+echo "Generating your group's availability</br>";
 ?>
 <html>
 	<?php
@@ -10,17 +10,27 @@ echo "Generating your group's availability";
 	?>
 	<body>
 		<?php
-			$stmt = $pdo->query('SELECT sum(avail.avail) as "Number of available" , days.day, time.Time, CASE
-		when sum(avail.avail) = 0 then "not available"
-  		 when sum(avail.avail)>0 then "available"
-    	end 
-   	as Availability
+			$stmt = $pdo->query('SELECT days.day, time.Time
+		-- 		, CASE
+		-- when sum(avail.avail) = 0 then "not available"
+  -- 		 when sum(avail.avail)>0 then "available"
+  --   	end 
+  --  	as Availability
 
 from avail join days on avail.day_id = days.day_id
 join time on avail.time_id = time.time_id
-group by avail.time_id, avail.day_id 
+group by avail.day_id, avail.time_id 
 having sum(avail.avail)>0');
+			// $sth->execute();
+			$rows = $stmt->fetchall(PDO::FETCH_ASSOC);
+// print_r($rows);
+			foreach ($rows as $row) {
+			// 	// echo sizeof($rows);
+				echo $row["day"]." ".$row['Time']."</br>";
+			}
+			// echo $rows[7];
 ?>
+
         
 	</body>
 </html>
