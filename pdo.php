3f81cbe3601f45db65c34af8395feb37bbd47bff
xihuang <huangxi@umich.edu>
2014-12-15 16:26:27 -0500

    update sql statement
    updated sql statement

--- pdo.php
+++ pdo.php
@@ -1,6 +1,7 @@
 <?php
-$pdo = new PDO('mysql:host=localhost;port=3306;dbname=meggiemo_scheduler', 
-   'meggiemo-fred', 'zap');
+
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=huangxi_scheduler', 
+  'huangxi_ugh', 'ugh1234');
 // See the "errors" folder for details...
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
